6edb9c6e24b16990b1cfa8eb8c8658c0b70880bb
Sauravrp67 <sp19061320@student.ku.edu.np>
2022-10-11 12:58:11 +0545

    EnterRaffle Test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,14 +5,15 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle", async function () {
-          let raffle, vrfCoordinatorV2Mock
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
           const chainId = network.config.chainId
 
           beforeEach(async function () {
-              const { deployer } = await getNamedAccounts
+              deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
           })
 
           describe("constructor", async function () {
@@ -30,6 +31,11 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       raffle,
                       "Raffle__NoEnoughETHEntered"
                   )
+              })
+              it("Records player when they enter", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayer(0)
+                  assert.equal(playerFromContract, deployer)
               })
           })
       })
